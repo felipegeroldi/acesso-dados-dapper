04b6321d3a4b915480dc738d5b1d893fab890b5a
Felipe Geroldi <42786299+felipegeroldi@users.noreply.github.com>
2022-05-03 21:44:12 -0300

    Dapper - QueryMultiple

--- Program.cs
+++ Program.cs
@@ -21,7 +21,8 @@ public class Program
             // ExecuteScalar(connection);
             // ReadView(connection);
             // OneToOne(connection);
-            OneToMany(connection);
+            // OneToMany(connection);
+            QueryMultiple(connection);
         }
     }
 
@@ -293,6 +294,27 @@ public class Program
             foreach (var item in career.Items)
             {
                 Console.WriteLine($" - {item.Title}");
+            }
+        }
+    }
+
+    static void QueryMultiple(SqlConnection connection)
+    {
+        var query = "SELECT * FROM [Category]; SELECT * FROM [Course]";
+
+        using (var multi = connection.QueryMultiple(query))
+        {
+            var categories = multi.Read<Category>();
+            var courses = multi.Read<Course>();
+
+            foreach (var item in categories)
+            {
+                Console.WriteLine(item.Title);
+            }
+
+            foreach (var item in courses)
+            {
+                Console.WriteLine(item.Title);
             }
         }
     }
